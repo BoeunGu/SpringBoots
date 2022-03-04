1d3be649fd09ff35600ec70c3faf954a030bcdd6
Boeun's <rnqhdms@gmail.com>
2022-03-04 13:42:59 +0900

    request, response

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,8 +1,10 @@
 package hello.springmvc.basic.request;
 
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -53,4 +56,53 @@ public class RequestParamController {
         log.info("username={},age={}", username, age);
         return "ok";
     }
+
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age){
+
+        log.info("username={},age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-defalut")
+    public String requestParamDefault(
+            @RequestParam(required = true,defaultValue = "guest") String username,
+            @RequestParam(required = false,defaultValue = "-1") Integer age){
+
+        log.info("username={},age={}", username, age);
+        return "ok";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String,Object>paramMap){
+
+        log.info("username={},age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+        log.info("username={}, age={}", helloData.getUsername(),helloData.getAge());
+                return "OK";
+
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    //@ResponseBody를 사용하면 응답결과를 HTTP 메세지 바디에 직접 담아서 전달할 수 있다.
+    public String modelAttributeV2(HelloData helloData){ //@ModelAttribute 생략 가능 (String, Integer,int는 @RequestParam으로 인식)
+        log.info("username={}, age={}", helloData.getUsername(),helloData.getAge());
+        return "OK";
+
+    }
+
+
+
 }
